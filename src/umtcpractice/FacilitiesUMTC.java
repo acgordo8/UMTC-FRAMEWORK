4f4769f3c9a6837c5ebc4e7de860dcbf688550b0
acgordo <acgordo8alex.gordo@dice205.com>
2019-07-17 09:07:44 +0800

    Facilities

--- src/umtcpractice/FacilitiesUMTC.java
+++ src/umtcpractice/FacilitiesUMTC.java
@@ -1,7 +1,12 @@
 package umtcpractice;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class FacilitiesUMTC extends VariablesAndBrowser {
@@ -19,27 +24,64 @@ public class FacilitiesUMTC extends VariablesAndBrowser {
 	}
 	
 	@Test(priority = 1,  dependsOnMethods= {"lumagda"})
-	public void Addfacilities() throws InterruptedException {
+	public void Addfacilities() throws InterruptedException, IOException {
 		
+		//clicking facilities in side bar
 		driver.findElement(By.linkText("Facilities")).click();
 		Thread.sleep(3000);
-		
+		//Click add new
 		driver.findElement(By.partialLinkText("Add")).click();
 		
-		
+		//Enter Title
 		driver.findElement(By.xpath("//*[@id=\"title\"]")).sendKeys(Keys.chord(Keys.CONTROL, "a"),"INFORMATION TECHNOLOGY");
 		Thread.sleep(3000);
+		//body text
+		driver.findElement(By.id("content")).sendKeys(Keys.chord(Keys.CONTROL, "a"),"Information Technology");
+		Thread.sleep(3000);
+		//Click Add new Category
+		driver.findElement(By.id("facility_category-add-toggle")).click();
+		Thread.sleep(5000);
+		//Category Name
+		driver.findElement(By.id("newfacility_category")).sendKeys(Keys.chord(Keys.CONTROL, "a"),"Information Technology");
+	
+		//Click Dropdown
+		WebElement Category = driver.findElement(By.id("newfacility_category_parent"));
+		Category.click();
+		Thread.sleep(3000);
+		
+		Select select = new Select (Category);
+		select.selectByVisibleText("Simulator");
+		
+		//Click add new Category
+		driver.findElement(By.id("facility_category-add-submit")).click();
+		Thread.sleep(3000);
+		
+		
+		
+		//Set Featured Image
 		driver.findElement(By.id("set-post-thumbnail")).click();
 		Thread.sleep(3000);
-		/*driver.findElement(By.linkText("Insert from URL")).click();*/
-		
-		driver.findElement(By.cssSelector("#__wp-uploader-id-0 > div.media-frame-router > div > a.media-menu-item.active")).click();
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("//*[@id=\"__attachments-view-1417\"]/li[6]/div")).click();
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("//*[@id=\"__wp-uploader-id-0\"]/div[5]/div/div[2]/button")).click();
+		//Click Upload Files
+		driver.findElement(By.partialLinkText("Upload")).click();
 		Thread.sleep(3000);
-		driver.findElement(By.xpath("//*[@id=\"post-preview\"]")).click();
+		//Select Files
+		driver.findElement(By.id("__wp-uploader-id-1")).click();
+		//AutoIT tools to select specific files or images in desktop/laptop
+		Runtime.getRuntime().exec("C:\\Users\\DICE205\\Documents\\Facilities.exe");
+		Thread.sleep(5000);
+		//Click set Featured Image
+		driver.findElement(By.cssSelector("#__wp-uploader-id-0 > div.media-frame-toolbar > div > div.media-toolbar-primary.search-form > button")).click();
+		System.out.println("Successfully Uploaded");
+		
+		Thread.sleep(5000);
+		 
+		//Click Publish
+		driver.findElement(By.cssSelector("#publish")).click();
+		Thread.sleep(5000);
+		//Click Post-Review
+		driver.findElement(By.cssSelector("#post-preview")).click();
+		
+		
 	}
 }
 
